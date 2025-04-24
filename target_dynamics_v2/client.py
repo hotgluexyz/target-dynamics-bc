a91a0a240bee2722ca467d5fccb1186244a5fbb9
Vinicius Ghellere Sbardelotto <vinicius@hotglue.io>
2025-04-24 11:19:59 -0300

    feat: implement DynamicClient and auth

--- target_dynamics_v2/client.py
+++ target_dynamics_v2/client.py
@@ -1,61 +1,85 @@
-from target_hotglue.client import HotglueSink
+import json
 import requests
+
+from target_hotglue.client import HotglueSink
+from target_hotglue.common import HGJSONEncoder
+from target_dynamics_v2.utils import Company
 from singer_sdk.plugin_base import PluginBase
 from typing import Dict, List, Optional
 import singer
+
+
 from target_dynamics_v2.auth import DynamicsAuth
+
 LOGGER = singer.get_logger()
 
-class DynamicsSink(HotglueSink):
-    """Dynamics target sink class."""
-    def __init__(
-        self,
-        target: PluginBase,
-        stream_name: str,
-        schema: Dict,
-        key_properties: Optional[List[str]],
-    ) -> None:
-        super().__init__(target, stream_name, schema, key_properties)
-        # Save config for refresh_token saving
-        self.config_file = target.config
-        self.target_name = "dynamics"
-        if self.config.get("full_url"):
-            base_url = self.config["full_url"]
-        else:
-            base_url = "https://{}.crm.dynamics.com".format(self.config["org"])
-        
-        if base_url.endswith("/"):
-            base_url = base_url[:-1]
-        
-        self.url = f"{base_url}/api/data/v9.2/"
-        self.entityid = None
+class DynamicsClient:
+    ref_request_endpoints = {
+        "companies": "companies",
+        "currencies": "companies({companyId})/currencies",
+        "paymentMethods": "companies({companyId})/paymentMethods"
+    }
+
+    def __init__(self, config) -> None:
+        self.config = config
+        environment = self.config.get("environment_name")
+        self.url = self.config.get("full_url", f"https://api.businesscentral.dynamics.com/v2.0/{environment}/api/v2.0/")
+        self.auth = DynamicsAuth(dict(self.config))
 
     def get_auth(self):
-        auth = DynamicsAuth(dict(self.config))
         r = requests.Session()
-        auth = auth(r)
-        return auth
+        return self.auth(r)
+    
+    def _make_request(self, endpoint, method, data=None, params=None, headers=None):
+        request_headers = {"Content-Type": "application/json"}
+        if headers:
+            request_headers.update(headers)
 
-    def log_request_response(self, record, response):
-        self.logger.info(f"Sending payload for stream {self.name}: {record}")
-        self.logger.info(f"Response: {response.text}")
+        url = self.url + endpoint
+        request_params = params or {}
 
-    def upsert_record(self, record: dict, context: dict):
-        method = "POST"
-        state_dict = dict()
-        id = None
-        endpoint = self.endpoint
-        auth = self.get_auth()
-        url =  self.url
-        url = f"{url}{endpoint}"
-        auth.headers.update({"OData-MaxVersion":"4.0","OData-Version":"4.0","Accept":"application/json","Content-Type":"application/json","charset":"utf-8","Prefer":"return=representation"})
-        response = auth.post(url,json=record)
-        self.validate_response(response)
-        self.log_request_response(record, response)
-        if response.status_code in [200, 201]:
-            state_dict["success"] = True
-            id = response.json().get(self.entityid)
-        #Updating records doesn't seem to work
-        elif response.status_code == 204 and method == "PUT":
-            state_dict["is_updated"] = True
-        return id, response.ok, state_dict        
+        request = self.get_auth()
+        request.headers.update(request_headers)
+        request.request(method=method, url=url, params=params)
+
+        json_data = json.dumps(data, cls=HGJSONEncoder) if data else None
+
+        return request.request(
+            method=method,
+            url=url,
+            params=request_params,
+            data=json_data,
+            verify=True
+        )
+    
+    def _validate_response(self, response: requests.Response) -> tuple[bool, str | None]:
+        if response.status_code >= 400:
+            msg = self._response_error_message(response)
+            return False, msg
+        else:
+            return True, None
+
+    def get_reference_data(self, record_type: str, url_params: Optional[dict] = {}):
+        endpoint = self.ref_request_endpoints[record_type].format(**url_params)
+        response = self._make_request(endpoint, "GET")
+
+        success, error_message = self._validate_response(response)
+        if not success:
+            return success, error_message, []
+        
+        resp_json = response.json()
+        return True, None, resp_json.get("value", [])
+
+    def get_companies(self):
+        _, _, companies = self.get_reference_data("companies")
+
+        for company in companies:
+            url_params = {"companyId": company["id"]}
+
+            _, _, currencies = self.get_reference_data("currencies", url_params)
+            company["currencies"] = currencies
+            
+            _, _, payment_methods = self.get_reference_data("paymentMethods", url_params)
+            company["paymentMethods"] = payment_methods
+
+        return True, None, companies
