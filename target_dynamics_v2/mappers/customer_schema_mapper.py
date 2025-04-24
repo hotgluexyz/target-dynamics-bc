f7d4b5faf5d8fd50212ea5c03022fe6773a5b412
Vinicius Ghellere Sbardelotto <vinicius@hotglue.io>
2025-04-24 11:21:20 -0300

    feat: implement customerSchemaMapper

---
+++ target_dynamics_v2/mappers/customer_schema_mapper.py
@@ -0,0 +1,52 @@
+from target_dynamics_v2.mappers.base_mappers import BaseMapper
+
+class CustomerSchemaMapper(BaseMapper):
+    field_mappings = {
+        "fullName": "displayName",
+        "email": "email",
+        "website": "website",
+        "taxable": "taxLiable"
+    }
+
+    def to_dynamics(self) -> dict:
+        
+        # TODO: move this to the sink
+        request_params = {
+            "url": "customers",
+            "method": "POST",
+            "headers": {
+                "Company": self.company["name"],
+            }
+        }
+
+        payload = {
+            "blocked": " ",
+            "type": "Person" if self.record.get("isPerson") else "Company",
+            **self._map_payment_method(),
+        }
+
+        # map phoneNumbers
+        # map addresses
+        # map currency
+
+        # parentId / parentName (dimensions)
+        # map categoryId / categoryName (dimensions)
+
+        self._map_fields(payload)
+        return {"payload": payload, "request_params": request_params }
+    
+    def _map_payment_method(self):
+        if self.company is None:
+            return {}
+        
+        found = None
+        if payment_method := self.record.get("paymentMethod"):
+            found = next(
+                (item for item in self.company.get("paymentMethods", []) if item["id"] == payment_method or item["code"] == payment_method or item["displayName"] == payment_method),
+                None
+            )
+
+            if found:
+                return {"paymentMethodId": found["id"]}
+
+        return {}
