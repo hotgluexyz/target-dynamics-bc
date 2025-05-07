5d25c86d1120eebbf3fb80a057139d8c810387fd
Vinicius Ghellere Sbardelotto <vinicius@hotglue.io>
2025-05-07 08:53:51 -0300

    feat: implement BillExpenseItemSchemaMapper

---
+++ target_dynamics_v2/mappers/bill_expense_item_schema_mapper.py
@@ -0,0 +1,63 @@
+from target_dynamics_v2.mappers.base_mappers import BaseMapper
+from target_dynamics_v2.utils import RecordNotFound
+
+class BillExpenseItemSchemaMapper(BaseMapper):
+    name = "BillLines"
+    field_mappings = {
+        "externalId": "sequence",
+        "description": "description",
+        "taxCode": "taxCode",
+        "discount": "discountAmount",
+        "amount": "unitCost",
+    }
+
+    def __init__(
+            self,
+            record,
+            sink,
+            reference_data,
+            existing_lines,
+            mapped_parent_record
+    ) -> None:
+        self.existing_lines = existing_lines
+        super().__init__(record, sink, reference_data, mapped_parent_record=mapped_parent_record)
+
+    def to_netsuite(self) -> dict:
+        payload = {
+            **self._map_internal_id(),
+            **self._map_account(),
+            **self._map_location(),
+            **self._map_dimension_set_lines(),
+            "lineType": "Account",
+            "quantity": 1
+        }
+
+        self._map_fields(payload)
+
+        return payload
+
+    def _find_existing_record(self, reference_list):
+        """Finds an existing record in the reference data by matching internal.
+        """
+        if self.company is None:
+            return None
+        
+        found_record = None
+
+        if record_external_id := self.record.get("externalId"):
+            found_record = next(
+                (line for line in self.existing_lines
+                if line["sequence"] == record_external_id),
+                None
+            )
+
+        record_account = self._map_account()
+        if (record_account_id := record_account.get("accountId")) and (record_description := self.record.get("description")):
+            found_record = next(
+                (line for line in self.existing_lines
+                if line["description"] == record_description and line["accountId"] == record_account_id),
+                None
+            )
+
+        if found_record:
+            return found_record
