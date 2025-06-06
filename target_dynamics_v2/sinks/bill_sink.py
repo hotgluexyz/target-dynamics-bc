056be95e0a803cd55c1baf9a7074dbb7d8f2cae9
Vinicius Ghellere Sbardelotto <vinicius@hotglue.io>
2025-06-06 16:07:06 -0300

    fix: bill mappings

--- target_dynamics_v2/sinks/bill_sink.py
+++ target_dynamics_v2/sinks/bill_sink.py
@@ -16,7 +16,7 @@ class BillSink(DynamicsBaseBatchSinkSingleUpsert):
         bill_filter_mappings = [
             {"field_from": "id", "field_to": "id", "should_quote": False},
             {"field_from": "transactionNumber", "field_to": "number", "should_quote": True},
-            {"field_from": "externalId", "field_to": "vendorInvoiceNumber", "should_quote": True},
+            {"field_from": "billNumber", "field_to": "vendorInvoiceNumber", "should_quote": True},
         ]
         existing_company_bills = self.dynamics_client.get_existing_entities_for_records(
             self._target.reference_data.get("companies", []),
@@ -29,7 +29,7 @@ class BillSink(DynamicsBaseBatchSinkSingleUpsert):
         # get vendors for company, filter by id, number, displayName
         vendor_filter_mappings = [
             {"field_from": "vendorId", "field_to": "id", "should_quote": False},
-            {"field_from": "vendorExternalId", "field_to": "number", "should_quote": True},
+            {"field_from": "vendorNumber", "field_to": "number", "should_quote": True},
             {"field_from": "vendorName", "field_to": "displayName", "should_quote": True},
         ]
         existing_company_vendors = self.dynamics_client.get_existing_entities_for_records(
@@ -43,13 +43,13 @@ class BillSink(DynamicsBaseBatchSinkSingleUpsert):
         items_set = set()
         # remove duplicated items across all records
         for record in records:
-            items_set.update((line_item.get("itemId"), line_item.get("itemExternalId"), line_item.get("itemName"), record.get("subsidiaryId"), record.get("subsidiaryName")) for line_item in record.get("lineItems", []))
+            items_set.update((line_item.get("itemId"), line_item.get("itemNumber"), line_item.get("itemName"), record.get("subsidiaryId"), record.get("subsidiaryName")) for line_item in record.get("lineItems", []))
         # make a list of unique items
-        items = [{"itemId": item[0], "itemExternalId": item[1], "itemName": item[2], "subsidiaryId": item[3], "subsidiaryName": item[4]} for item in items_set]
-        sorted_items = sorted(items, key=lambda item: (item.get("itemId"), item.get("itemExternalId"), item.get("itemName"), item.get("subsidiaryId"), item.get("subsidiaryName")))
+        items = [{"itemId": item[0], "itemNumber": item[1], "itemName": item[2], "subsidiaryId": item[3], "subsidiaryName": item[4]} for item in items_set]
+        sorted_items = sorted(items, key=lambda item: (item.get("itemId"), item.get("itemNumber"), item.get("itemName"), item.get("subsidiaryId"), item.get("subsidiaryName")))
         item_filter_mappings = [
             {"field_from": "itemId", "field_to": "id", "should_quote": False},
-            {"field_from": "itemExternalId", "field_to": "number", "should_quote": True},
+            {"field_from": "itemNumber", "field_to": "number", "should_quote": True},
             {"field_from": "itemName", "field_to": "displayName", "should_quote": True},
         ]
         existing_company_items = self.dynamics_client.get_existing_entities_for_records(
@@ -59,7 +59,12 @@ class BillSink(DynamicsBaseBatchSinkSingleUpsert):
             item_filter_mappings
         ) if items else []
 
-        self.reference_data = {**self._target.reference_data, self.name: existing_company_bills, "Vendors": existing_company_vendors, "Items": existing_company_items}
+        self.reference_data = {
+            **self._target.reference_data,
+            self.name: existing_company_bills,
+            "Vendors": existing_company_vendors,
+            "Items": existing_company_items
+        }
 
     def process_batch_record(self, record: dict) -> dict:
         # perform the mapping
